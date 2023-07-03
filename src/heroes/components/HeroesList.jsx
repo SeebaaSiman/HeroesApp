7dc967afe96b74045bb75fc6a73befb62d75f929
Sebastian <seebaasiman@gmail.com>
2023-07-03 03:40:18 -0300

    Terminado el último UI

--- src/heroes/components/HeroesList.jsx
+++ src/heroes/components/HeroesList.jsx
@@ -1,17 +1,19 @@
 import { HeroCard } from "./";
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
 import { useMemo } from "react";
+import { HeroCards } from "./HeroCards";
 
 export const HeroesList = ({ publisher }) => {
   const heroe = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <>
-      <h1 className="animate__animated animate__jello">{publisher}</h1>
+      <h1 className="mb-4 animate__animated animate__jello">{publisher}</h1>
 
       <div className="row rows-cols-1 row-cols-md-3 g-3">
         {heroe.map((hero) => (
-          <HeroCard key={hero.id} {...hero} />
+          // <HeroCard key={hero.id} {...hero} />
+          <HeroCards key={hero.id} {...hero} />
         ))}
       </div>
     </>
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../auth";
+import { styled } from "styled-components";
 
 export const Navbar = () => {
   //Traigo aquí por contexto el user, será user. name para saber el nombre del user//
@@ -15,46 +16,80 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className=" navbar navbar-expand-sm navbar-dark bg-dark p-2">
-      <Link className="navbar-brand" to="/">
-        Asociaciones
-      </Link>
-      <div className="navbar-collapse">
-        <div className="navbar-nav">
-          <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? "active" : ""}`
-            }
-            to="/marvel"
-          >
-            Marvel
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => `
-          nav-item nav-link ${isActive ? "active" : ""}
-          `}
-            to="/dc"
-          >
-            DC
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => `
-          nav-item nav-link ${isActive ? "active" : ""}
-          `}
-            to="/search"
-          >
-            Search
-          </NavLink>
-        </div>
-      </div>
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-        <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary"> {user?.name} </span>
-          <button className="nav-item nav-link btn" onClick={onLogout}>
-            Logout
-          </button>
-        </ul>
-      </div>
-    </nav>
+    <NavBarContainer>
+      <Header>
+        <Links to="/">Asociaciones</Links>
+        <LinksContainer>
+          <NavLinks to="/marvel">Marvel</NavLinks>
+          <NavLinks to="/dc">DC</NavLinks>
+          <NavLinks to="/search">Search</NavLinks>
+        </LinksContainer>
+      </Header>
+
+      <Footer>
+        <span>User login: {user?.name}</span>
+        <Button onClick={onLogout}>Logout</Button>
+      </Footer>
+    </NavBarContainer>
   );
 };
+
+const NavBarContainer = styled.div`
+  background-color: #222;
+  font-size: 1.2rem;
+`;
+const Header = styled.div`
+  display: flex;
+  padding-bottom: 0.5rem;
+`;
+const Links = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+`;
+const LinksContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const NavLinks = styled(NavLink)`
+  color: #fff;
+  text-decoration: none;
+  &:hover {
+    color: #000;
+  }
+  &.active {
+    color: #1a31b8;
+  }
+`;
+const Footer = styled.div`
+  display: flex;
+  padding-bottom: 0.5rem;
+  justify-content: space-around;
+  align-items: center;
+  span {
+    color: #fff;
+  }
+`;
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  cursor: pointer;
+  width: 150px;
+  height: 50px;
+  background-image: linear-gradient(to top, #d8d9db 0%, #fff 80%, #fdfdfd 100%);
+  border-radius: 30px;
+  border: 1px solid #8f9092;
+  transition: all 0.2s ease-in-out;
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 14px;
+  font-weight: 600;
+  color: #606060;
+  text-shadow: 0 1px #fff;
+  &:hover {
+    box-shadow: 0 4px 3px 1px #fcfcfc, 0 6px 8px #d6d7d9, 0 -4px 4px #cecfd1,
+      0 -6px 4px #fefefe, inset 0 0 3px 3px #cecfd1;
+  }
+`;
